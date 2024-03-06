1dfe9fbe69339ef16bb813803289a132e028d385
cherisys <arshad@cherisys.com>
2024-03-07 03:15:36 +0530

    Polly, MongoDB, MassTransit, RabbitMQ

---
+++ PlayEconomySystem/Play.Common/src/Play.Common/RabbitMQ/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Play.Common.Settings;
+using System;
+using System.Reflection;
+
+namespace Play.Common.RabbitMQ
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services)
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+                configure.UsingRabbitMq((context,configurator) =>
+                {
+                    var configuration = context.GetService<IConfiguration>();
+                    var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                    configurator.Host(rabbitMQSettings.Host);
+                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.Name,false));
+                    configurator.UseMessageRetry(retryConfigurator => 
+                    { 
+                        retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                    });
+                });
+            });
+
+            services.AddMassTransitHostedService();
+
+            return services;
+        }
+    }
+}
---
+++ PlayEconomySystem/Play.Inventory/src/Play.Inventory.Service/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Play.Inventory.Service.Entities;
+
+namespace Play.Inventory.Service
+{
+    public static class Extensions
+    {
+        public static InventoryItemDto AsDto(this InventoryItem item, string Name, string Description)
+        {
+            return new InventoryItemDto(item.CatalogItemId, Name, Description, item.Quantity, item.AcquiredDate);
+        }
+    }
+}
